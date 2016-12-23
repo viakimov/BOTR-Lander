23e115ff0bda625cc11994c4f4efae8c764bb755
viakimov <viakimovitch@gmail.com>
2016-12-23 17:01:53 -0500

    Added basic method layout

---
+++ BOTR_Lander_Software/BOTR_Lander_Software.ino
@@ -0,0 +1,47 @@
+void setup() 
+{
+ 
+
+}
+
+void loop() 
+{
+  //High level outline of lander operations, edit the methods below
+powerUp();
+integration();
+launch();
+deployment();
+landing();
+groundOperation();
+}
+
+void powerUp()
+{
+  
+}
+
+void integration()
+{
+  
+}
+
+void launch()
+{
+  
+}
+
+void deployment()
+{
+  
+}
+
+void landing()
+{
+  
+}
+
+void groundOperation()
+{
+  
+}
+
